bb998285e459fea5256b7cb4687ba106e628b130
vanshu778 <vthesiya778@gmail.com>
2025-01-31 22:30:37 +0530

    update

--- 5_functions.js
+++ 5_functions.js
@@ -164,25 +164,32 @@
 //! Question 1: Calculator Function
 //! Write a JavaScript function calculator that takes two numbers and an operator as parameters and returns the result of the operation. The function should support addition, subtraction, multiplication, and division.
 
-// function calculator(a, b, operator){
-//     let result;
-//     switch(operator){
-//         case "+":
-//             result= a+b;
-//             return result;
-        
-//         case "-":
-//             result = a-b;
-//             return result;
+// const calculator = (num1, num2, operator) => {
+//   let result;
+//   switch (operator) {
+//     case "+":
+//       return num1 + num2;
 
-//         case "/":
-//             result = a/b;
-//             return result;
+//     case "-":
+//       result = num1 - num2;
+//       return result;
+
+//     case "*":
+//       result = num1 * num2;
+//       return result;
+
+//     case "/":
+//       if (num2 === 0) {
+//         return "0 is not allowed";
+//       } else {
+//         result = num1 / num2;
+//         return result;
+//       }
 
 //     default:
-//         return "no operator found"; 
-//     }
-// }
+//       return "no operator found";
+//   }
+// };
 // console.log(calculator(5,2,"+"));
 // console.log(calculator(5,2,"-"));
 // console.log(calculator(5,0,"/"));
